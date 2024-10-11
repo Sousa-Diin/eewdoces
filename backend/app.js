3ddac736f2f1bf7752cc30567311c6192198c1f9
Wildes Sousa <augustowildes@gmail.com>
2024-10-11 11:24:38 -0300

    :rocket: test validados rotas funcionais

--- backend/app.js
+++ backend/app.js
@@ -1,33 +1,22 @@
 import 'dotenv/config';
 import express from 'express';
-import pool from '../backend/repository/db.js';
+import PeopleRoutes from './routes/peopleRoutes.js';
+import ProductsRoutes from './routes/productsRoutes.js';
+import OrderProductRoutes from './routes/orderProductRoutes.js';
+import OrdersRoutes from './routes/ordersRoutes.js';
+import TransactionsRoutes from './routes/transactionsRoutes.js';
 
 const app = express();
-const port = 4001;
+const port = process.env.WEB_PORT;
+const host = process.env.WEB_HOST;
 
-app.get('/', async (req,res) => {
-  const table = req.query.table;
-  const allowedTables = ['people','products','order_product','orders','transactions'];
-
-
-  if(!allowedTables.includes(table)){
-    return res.status(400).send('Tabela inválida');
-  }
-  
-  const textQuery = `SELECT * FROM ${table}`;
-
-  try {
-    const results = (await pool.query(textQuery)).rows
-    res.status(200).send(results);
-    
-  } catch (error) {
-    console.error('Erro ao executar a consulta', error);
-    res.status(500).send('Erro ao acessar os dados');
-  }
-  
-});
+app.use('/people', PeopleRoutes); 
+app.use('/products', ProductsRoutes); 
+app.use('/order-product', OrderProductRoutes); 
+app.use('/orders', OrdersRoutes); 
+app.use('/transactions', TransactionsRoutes); 
 
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port,host , () => {
   console.log('API da E&WDoces está online.');
 });
