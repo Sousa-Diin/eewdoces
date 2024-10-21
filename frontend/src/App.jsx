289a1a9383d6aa5c0deb227e589eea6f40c990cf
Wildes Sousa <augustowildes@gmail.com>
2024-10-21 14:06:18 -0300

    web/fixes #24 :construction: created windows for mobile and desktop | restruturacao da pasta front

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+import Login from './assets/Login.jsx';
+
+function App() {
+ 
+  return (
+    <div className="container">
+     <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/assets/Login.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import '../../src/App.css'
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Lógica para login
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit} className="login-form">
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Entrar</button>
+        <footer className='login-footer'><a>cadastrar</a><a>esqueci minha Senha</a></footer>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
