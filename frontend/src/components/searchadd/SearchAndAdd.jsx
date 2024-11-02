17335a9a2e458c6bcfd7c15b08996dd0ea1a1c09
Wildes Sousa <augustowildes@gmail.com>
2024-11-02 02:54:07 -0300

    style :hammer: configuracoes no css file

--- frontend/src/components/searchadd/SearchAndAdd.jsx
+++ frontend/src/components/searchadd/SearchAndAdd.jsx
@@ -5,14 +5,14 @@ import './searchandadd.css';
 import lupa from '../../assets/icons/lupa.svg';
 import circle_plus from '../../assets/icons/circle-plus.svg';
 
-const SearchAndAdd = ({id, placeholder}) => {
+const SearchAndAdd = ({id, placeholder, handleSearch, handleAdd}) => {
   return ( 
     <nav className='dad-search-container'>
       <div className='search-container'>
         <input type="text" placeholder={placeholder} /> 
-        <img src={lupa} alt="lupa search" />
+        <img onClick={handleSearch} src={lupa} alt="lupa search" />
       </div> 
-      <img src={circle_plus} alt=" add item" id={id} />
+      <img onClick={handleAdd} src={circle_plus} alt=" add item" id={id} />
     </nav>
   )
 }
--- frontend/src/pages/register/clientRegister/ClientRegister.jsx
+++ frontend/src/pages/register/clientRegister/ClientRegister.jsx
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react';
 
 import './clientregister.css';
 import arrow_left from '../../../assets/icons/chevron-left.svg';
-
 import ArrowLeft from '../../arrowleft/ArrowLeft';
-
+import SearchAndAdd from '../../../components/searchadd/SearchAndAdd'
+import SalesInfo from '../../../components/card/sales/SalesInfo';
 
 const ClientRegister = () => {
  
@@ -14,11 +14,11 @@ const ClientRegister = () => {
 
   
   return(
-    <div /* className='container-main-sales' */>
-    <ArrowLeft to={url} logo={arrow_left} style='around' >Cadastre um cliente</ArrowLeft>
+    <div className='container-main-sales-register'>
+      <ArrowLeft to={url} logo={arrow_left} >Cadastre um cliente</ArrowLeft>
       <main className='container-sales-register'>
-        {/* <SearchAndAdd id='search-sales' placeholder={'Buscar por vendas'}/> */}
-       
+        <SearchAndAdd id='search-sales' placeholder={'Buscar por nome do cliente'}/>
+        <SalesInfo arrayList={[]}/>
         
       </main>
       {/* <Navbar/> */}
