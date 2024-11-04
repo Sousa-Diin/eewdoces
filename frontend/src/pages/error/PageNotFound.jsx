4b112178745d43e31f305cacdd04c9fbb5691b1c
Wildes Sousa <augustowildes@gmail.com>
2024-11-04 06:44:05 -0300

    :rocket: add redirect page

--- frontend/src/pages/error/PageNotFound.jsx
+++ frontend/src/pages/error/PageNotFound.jsx
@@ -1,6 +1,12 @@
+import { useNavigate } from 'react-router-dom';
 import './pagenotfound.css';
 
 const PageNotFound = () => {
+  const navigate = useNavigate();
+  setTimeout(()=>{
+    navigate('/eewdoces/main/store');
+  },2000);
+
   return (
     <div className='container-not-found'>
       <h2>A página solicitada não foi encontrada</h2>
