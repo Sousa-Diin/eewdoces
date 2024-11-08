59197b597ebd53e9205a620961b85819b14b468b
Wildes Sousa <augustowildes@gmail.com>
2024-11-08 06:33:23 -0300

    style: change layout container item supplier

--- frontend/src/components/card/supplier/item/ItemSuppliers.jsx
+++ frontend/src/components/card/supplier/item/ItemSuppliers.jsx
@@ -5,10 +5,10 @@ import './itemsuppliers.css';
 const ItemSuppliers = ({item, index}) => {
   return(
     <div key={index} className="sales">
-      <div className='container-status'><span>N°{index+1}</span> <pre className="type-person">{item.type_person}</pre></div>
-      <h6>{item.name}</h6>
-      <span>Phone: {item.phone}</span>
-      <p>{item.address}</p>
+      <div className='container-status'><span>ID: {index+1}</span> <pre className="type-person">{item.type_person}</pre></div>
+      <h6 className="suppliers-name">{item.name}</h6>
+      <span className="suppliers-phone">Telefone: {item.phone}</span>
+      <span className="suppliers-adress">{item.address}</span>
     </div>
   )
 }
