6494478774915cc743dfa96e8caf70dcaeceae37
Wildes Sousa <augustowildes@gmail.com>
2024-11-16 06:15:23 -0300

    feature: :rocket: Fixes #36 add people form

---
+++ frontend/src/pages/register/forms/PeopleForm.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+import './forms.css';
+
+const PeopleForm = ({type, titleBtn}) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+    address: "",
+    type_person:"",
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [id]: value,
+      type_person: type,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Dados da pessoa:", formData);
+    // Aqui você pode enviar os dados para um backend ou API
+    alert("Pessoa salva com sucesso!");
+     // Limpar os campos do formulário
+    setFormData({
+      name: "",
+      phone: "",
+      address: "",
+    });
+  };
+  return (
+    <form className="people-form" onSubmit={handleSubmit}>
+      <div className="name">
+        <label htmlFor="name">Nome</label>
+        <input type="text" name="people" id="name" 
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="phone">
+        <label htmlFor="phone">Telefone </label>
+        <input type="tel" name="people" id="phone" 
+          value={formData.phone}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="address">
+        <label htmlFor="address">Endereço </label>
+        <input type="text" name="people" id="address" 
+          value={formData.address}
+          onChange={handleChange} />
+      </div>
+     
+      <button type="submit" id="register-client">{titleBtn}</button>
+    </form>
+  );
+}
+
+export default PeopleForm;
